960ff2e791322d7be73b60166247b7cf440b468e
Fabian Forero <fabianforero779@gmail.com>
2021-08-30 12:01:19 -0400

    Ajustes de implementacion del servidor con clases
    Ajustas para servir la aplicacion express por medio de la nueva implementacion de clase.

--- app.js
+++ app.js
@@ -1,54 +1,8 @@
 require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const exphbs = require('express-handlebars');
+const Server = require('./src/class/server');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var server = new Server();
 
-var app = express();
-
-/* config */
-const hbs = exphbs.create({
-  defaultLayout: 'main.hbs',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/partials',
-  extname: 'hbs',
-});
-
-const hbsName = 'hbs';
-//Sets handlebars configurations (we will go through them later on)
-app.engine(hbsName, hbs.engine);
-//Sets our app to use the handlebars engine
-app.set('view engine', hbsName);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', {title: '404-Error'});
-});
+var app = server.app;
 
 module.exports = app;
