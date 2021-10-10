24fb3e05cf9ba6f226d1242fffdfeb1761ada55e
Fabian Forero <fabianforero779@gmail.com>
2021-10-10 14:39:56 -0400

    Implementation of validations at user routes
    were applied validations of user routes, checking to mongoose object id and input data from the request body

--- src/apiModules/users/routes.js
+++ src/apiModules/users/routes.js
@@ -1,17 +1,21 @@
 const {Router} = require('express');
 
+const {userValidateCreate} = require('./validators/user-create.validator');
+const {userValidateUpdate} = require('./validators/user-edit.validator');
+const {mongooseValidateObjecID} = require('../../middleware/mongoose-validators.middleware');
+
 const {getUser, getUserList, createUser, updateUser, deleteUser} = require('./controller');
 
 const router = Router();
 
 router.get('/', getUserList);
 
-router.get('/:id', getUser);
+router.get('/:id', [mongooseValidateObjecID], getUser);
 
-router.post('/', createUser);
+router.post('/', userValidateCreate, createUser);
 
-router.put('/:id', updateUser);
+router.put('/:id', [mongooseValidateObjecID, userValidateUpdate], updateUser);
 
-router.delete('/:id', deleteUser);
+router.delete('/:id', [mongooseValidateObjecID], deleteUser);
 
 module.exports = router;
