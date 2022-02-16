148fbabf6c7ddd66d05c79dcbcaf7771a6b908a1
Fabian Forero <fabianforero779@gmail.com>
2022-02-15 21:12:28 -0500

    Include auth.routes in api

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 
 const router = Router();
 
+router.use('/auth', require('../../apiModules/auth/auth.routes'));
 router.use('/roles', require('../../apiModules/roles/role.routes'));
 router.use('/users', require('../../apiModules/users/user.routes'));
 
