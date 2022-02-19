73be44ab42dbebd46decb477cc746af597f9c00b
Fabian Forero <fabianforero779@gmail.com>
2022-02-19 11:00:00 -0500

    Implement middleware validateJwt
    Implement validateJWT on routes.
        => roles
        => users

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,8 +1,12 @@
 const {Router} = require('express');
+const {validateJWT} = require('../../middleware/validateJWT.middleware');
 
 const router = Router();
 
 router.use('/auth', require('../../apiModules/auth/auth.routes'));
+
+router.use(validateJWT);
+
 router.use('/roles', require('../../apiModules/roles/role.routes'));
 router.use('/users', require('../../apiModules/users/user.routes'));
 
