f613d71658e1cbcb0dd2ef8a2cab00dc5f964bda
Fabian Forero <fabianforero779@gmail.com>
2022-02-20 12:18:26 -0500

    Add google template.
    Add new route in web routes
        => google-sign-in to reder the template to login with google accounts.
    Add google template to test google sign in.
    Add new item in navbar => Google Sign In.

--- src/routes/web/index.js
+++ src/routes/web/index.js
@@ -1,17 +1,25 @@
 const {Router} = require('express');
 
+const {
+    google: {client_id},
+} = require('../../../config/config');
+
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Main App'});
+    res.render('index', {title: 'Main App'});
 });
 
 router.get('/generic', (req, res) => {
-  res.render('generic', {title: 'Main App'});
+    res.render('generic', {title: 'Main App'});
 });
 
 router.get('/elements', (req, res) => {
-  res.render('elements', {title: 'Main App'});
+    res.render('elements', {title: 'Main App'});
+});
+
+router.get('/google-sign-in', (req, res) => {
+    res.render('google', {title: 'Google Sign In', client_id});
 });
 
 module.exports = router;
