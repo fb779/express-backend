4e24288980b8c85db1a98d537a2d74d21c6255cc
Fabian Forero <fabianforero779@gmail.com>
2022-02-26 09:00:21 -0500

    Include category route on API

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -8,6 +8,7 @@ router.use('/auth', require('../../apiModules/auth/auth.routes'));
 
 router.use(validateJWT);
 
+router.use('/categories', require('../../apiModules/categories/categories.routes'));
 router.use('/roles', require('../../apiModules/roles/role.routes'));
 router.use('/users', require('../../apiModules/users/user.routes'));
 
