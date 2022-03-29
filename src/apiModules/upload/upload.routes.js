0fef5c943078999e9dae8ed93725019ac6e5a2e8
Fabian Forero <fabianforero779@gmail.com>
2022-03-28 23:04:08 -0400

    Add Module Upload
    Add module upload, complete

--- src/apiModules/upload/upload.routes.js
+++ src/apiModules/upload/upload.routes.js
@@ -1,10 +1,34 @@
 const {Router} = require('express');
+const fileUpload = require('express-fileupload');
+
+const {
+    upload: {uploadPath, uploadTmpPath, fileSize, pathTemp},
+} = require('./../../../config/config');
+
+const {isValidFile, uploadFields} = require('./validators/upload.validators');
+const {uploadServer, getImageById} = require('./upload.controller');
 
 const router = Router();
 
-router.use((req, res, next) => {
-    console.log(`Router of upload files`);
-    next();
-});
+router.use(
+    fileUpload({
+        createParentPath: true,
+        safeFileNames: true,
+        preserveExtension: 4,
+        abortOnLimit: true,
+        // responseOnLimit: `archivo muy grande`,
+        // limitHandler: (req, res) => {
+        //     console.log('hola desde el manejador del error de tamaño');
+        //     // next();
+        // },
+        useTempFiles: pathTemp,
+        tempFileDir: uploadTmpPath,
+        limits: {fileSize},
+    })
+);
+
+router.get('', uploadFields, getImageById);
+
+router.post('', [...uploadFields, isValidFile({name: 'image', ext: ['jpg', 'jpeg', 'png']})], uploadServer);
 
 module.exports = router;
