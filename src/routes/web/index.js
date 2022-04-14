5b251f383158bfdf4af5ce3f29081e0885c16066
Fabian Forero <fabianforero779@gmail.com>
2022-04-14 11:58:58 -0400

    Add url for static page and define static resources.
    Add the url for the chat web page and define the scripts to load on the web page.

--- src/routes/web/index.js
+++ src/routes/web/index.js
@@ -18,6 +18,12 @@ router.get('/elements', (req, res) => {
     res.render('elements', {title: 'Main App'});
 });
 
+router.get('/chat', (req, res) => {
+    const title = 'Chat';
+    const scripts = ['chat-socket.js'];
+    res.render('chat', {title, scripts});
+});
+
 router.get('/google-sign-in', (req, res) => {
     res.render('google', {title: 'Google Sign In', client_id});
 });
