8a07dd9e915f548c0266ce3c462bb8821417aa74
Fabian Forero <fabianforero779@gmail.com>
2022-04-15 11:19:24 -0400

    Add new resources for tickets exercise.
    Add new static resources
    Add new templates and corresponding js files for eache statics pages.

---
+++ public/assets/js/ticket-new.js
@@ -0,0 +1,9 @@
+const ticket_sk = io('/tickets');
+
+ticket_sk.on('connect', () => {
+    console.log(`conectado al servidor de tickets`);
+});
+
+ticket_sk.on('disconnect', () => {
+    console.log(`desconectado del servidor de tickets`);
+});
