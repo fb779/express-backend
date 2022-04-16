bbe358974674ac3b505c69e68a8f8ec16889fc1d
Fabian Forero <fabianforero779@gmail.com>
2022-04-16 06:46:22 -0400

    Add frontend behavior of create new ticket
    Adjusted ids and classes of elements on the template.
    Added behavior of button and lable when the client request a new ticket:
        => define the socket events to requested and responsed a new ticket.
        => define the actions of the html elements (listener).

--- public/assets/js/ticket-new.js
+++ public/assets/js/ticket-new.js
@@ -1,9 +1,25 @@
 const ticket_sk = io('/tickets');
 
+const lblNewTicket = document.querySelector('#lblNewTicket');
+const BtnNewTicket = document.querySelector('button');
+
 ticket_sk.on('connect', () => {
-    console.log(`conectado al servidor de tickets`);
+    BtnNewTicket.disabled = false;
 });
 
 ticket_sk.on('disconnect', () => {
-    console.log(`desconectado del servidor de tickets`);
+    BtnNewTicket.disabled = true;
+});
+
+ticket_sk.on('last-ticket', (payload) => {
+    lblNewTicket.innerHTML = payload;
+});
+
+BtnNewTicket.addEventListener('click', () => {
+    ticket_sk.emit('tk-next', null, (payload) => {
+        lblNewTicket.innerHTML = `Cargando...`;
+        setTimeout(() => {
+            lblNewTicket.innerHTML = payload;
+        }, 100);
+    });
 });
