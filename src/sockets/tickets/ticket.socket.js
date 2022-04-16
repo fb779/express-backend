bda5274f925154cce056c53c47dd1ffaaddfd692
Fabian Forero <fabianforero779@gmail.com>
2022-04-16 07:09:55 -0400

    Add implementation of events to creane new ticket.
    Add socket events to create a new ticket and emit the initial information.

--- src/sockets/tickets/ticket.socket.js
+++ src/sockets/tickets/ticket.socket.js
@@ -4,10 +4,20 @@ const ticketCtrl = new TicketControl();
 
 module.exports = {
     TicketSocketController: (client, io) => {
-        console.log('conectado al socket de tickets: ', client.id);
+        // client.on('disconnect', () => {
+        //     console.log('desconectado del socket de tickets: ', client.id);
+        // });
 
-        client.on('disconnect', () => {
-            console.log('desconectado del socket de tickets: ', client.id);
+        // TODO: carga inicial del ultimo ticket creado
+        client.emit('last-ticket', ticketCtrl.last ? `Ticket: ${ticketCtrl.last}` : `Without Tickets...`);
+
+        // TODO: event of new ticket
+        client.on('tk-next', (payload, cb) => {
+            const next_ticket = ticketCtrl.nextTicket();
+
+            cb && cb(next_ticket);
+
+            // HACK: Notificar la creacion de un nuevo ticket
         });
     },
 };
