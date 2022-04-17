67cfcba972086f53033abe832f78c7d5e612390e
Fabian Forero <fabianforero779@gmail.com>
2022-04-17 10:18:26 -0400

    Add: definition of events to the screens behavior.
    Events on:
        => tk-new   -> event to handle the creation of new ticket.
        => tk-next  -> event to handle the attend the next ticket.
    Events emit:
        => last-ticket      -> emit the number of last ticket.
        => total-ticket     -> emit the number of total tickets.
        => last-four        -> emit the last four tickets.

--- src/sockets/tickets/ticket.socket.js
+++ src/sockets/tickets/ticket.socket.js
@@ -3,21 +3,62 @@ const TicketControl = require('../../class/tickets-control');
 const ticketCtrl = new TicketControl();
 
 module.exports = {
-    TicketSocketController: (client, io) => {
+    TicketSocketController: (client, sk) => {
         // client.on('disconnect', () => {
-        //     console.log('desconectado del socket de tickets: ', client.id);
+        //     delete users[client.id];
         // });
 
-        // TODO: carga inicial del ultimo ticket creado
+        const totalTickets = () => {
+            // TODO: emision cantidad total de tickets al socket
+            sk.emit('total-ticket', ticketCtrl.tickets.length ? `${ticketCtrl.tickets.length}` : `Without Tickets...`);
+        };
+
+        const lastFour = () => {
+            // TODO: emision de los ultimos cuatro al socket
+            sk.emit('last-four', ticketCtrl.lastFour);
+        };
+
+        /**
+         * Emisiones iniciales al conectarse un nuevo cliente
+         */
+        // TODO: emision del ultimo ticket creado
         client.emit('last-ticket', ticketCtrl.last ? `Ticket: ${ticketCtrl.last}` : `Without Tickets...`);
+
+        totalTickets();
+
+        lastFour();
+
+        // TODO: event of new ticket
+        client.on('tk-new', (payload, cb) => {
+            const new_ticket = ticketCtrl.nextTicket();
+
+            cb && cb(new_ticket);
+
+            // TODO: Notificar la creacion de un nuevo ticket
+            totalTickets();
+        });
 
         // TODO: event of new ticket
         client.on('tk-next', (payload, cb) => {
-            const next_ticket = ticketCtrl.nextTicket();
+            const {dsk = null} = payload;
 
-            cb && cb(next_ticket);
+            if (!dsk) {
+                return cb && cb({ok: false, msg: `Desktop is required`});
+            }
 
-            // HACK: Notificar la creacion de un nuevo ticket
+            const next_ticket = ticketCtrl.attend(dsk);
+
+            if (!next_ticket) {
+                cb && cb(Object.assign({}, {ticket: {number: 'NA', uid: 'NA'}}));
+            } else {
+                cb && cb(Object.assign({}, {ticket: next_ticket}));
+            }
+
+            // TODO: Notificar la creacion de un nuevo ticket
+            totalTickets();
+
+            // TODO: Notificar los ultimos cuatro tickets
+            lastFour();
         });
     },
 };
