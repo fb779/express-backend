42fc342231009df823971c07e02398c200265a5c
Fabian Forero <fabianforero779@gmail.com>
2022-04-20 17:38:00 -0400

    Add: Definition of chat event on chat socket.
    Implementation of class ChatControl.
    Add the chat events:
        => emit -> chat-users-enable.
        => emit -> chat-list-messages.
        => on -> send-message.

--- src/sockets/chat/chat.socket.js
+++ src/sockets/chat/chat.socket.js
@@ -8,6 +8,10 @@ const {checkJWT} = require('../../helpers/jwt.helper');
 
 const {getUserById} = require('../../apiModules/users/user.dao');
 
+const ChatControl = require('../../class/chat-control');
+
+const chatControl = new ChatControl();
+
 const validateSoketJWT = async (token) => {
     try {
         const {uid} = await checkJWT(token);
@@ -19,6 +23,13 @@ const validateSoketJWT = async (token) => {
 
 module.exports = {
     ChatSocketController: async (client = new Socket(), sk) => {
+        const emitListUsers = () => {
+            sk.emit('chat-users-enable', chatControl.listUsers);
+        };
+
+        const emitListMessages = () => {
+            sk.emit('chat-list-messages', chatControl.lastTenMessages);
+        };
         // TODO: verificar el JWT
         const user = await validateSoketJWT(client.handshake.headers[headerToken]);
 
@@ -27,5 +38,23 @@ module.exports = {
         }
 
         console.log(`Cliente conectado: ${client.id} - user: ${user.first_name}`);
+
+        chatControl.addUser(user, client.id);
+        // TODO: Conectarce a su propia sala privada
+        client.join(user.uid);
+
+        emitListUsers();
+
+        client.on('send-message', ({message, to = null}, cb) => {
+            chatControl.addMessage(user.uid, user.first_name, message, to);
+            // cb && cb(true);
+            emitListMessages();
+        });
+
+        client.on('disconnect', () => {
+            client.leave(user.uid);
+            chatControl.removeUser(user.uid);
+            emitListUsers();
+        });
     },
 };
