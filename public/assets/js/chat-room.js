b9038f1d7c4acd3e3c69c8dc8c301d17851315f9
Fabian Forero <fabianforero779@gmail.com>
2022-04-20 17:41:15 -0400

    Add: improve the view of chat.
    Improve the UI for the web-chat roome

--- public/assets/js/chat-room.js
+++ public/assets/js/chat-room.js
@@ -1,5 +1,9 @@
 let user = null;
 
+const listUsers = document.querySelector('#listUsers');
+const msg = document.querySelector('#msg');
+const message = document.querySelector('#message');
+
 const checkLoginUser = () => {
     const token = localStorage.getItem('token');
     const urlAuth = `${window.location.origin}/api/auth`;
@@ -49,6 +53,69 @@ const connectSocket = () => {
     socket.on('disconnect', () => {
         console.log(`desconectado del servidor`);
     });
+
+    socket.on('chat-users-enable', drawUsers);
+
+    socket.on('chat-list-messages', drawMessages);
+
+    /**
+     * Events of html
+     */
+    message.addEventListener('keyup', (ev) => {
+        if (ev.keyCode === 13) {
+            const message = ev.target.value.trim();
+            const to = null;
+            socket.emit('send-message', {message, to});
+            ev.target.value = '';
+        }
+    });
+};
+
+const drawMessages = (ltMessages = []) => {
+    console.log('llegada de los mensajes', ltMessages);
+
+    let msgHtml = '';
+
+    for (const el of ltMessages) {
+        const date = new Date(el.date);
+        msgHtml += `
+            <div class="alert alert-info" role="alert">
+                <p>${el.message}</p>
+                <hr>
+                <p>
+                    <label class="text-success">
+                        ${el.name}
+                    </label>
+                    <span class="text-dark text-opacity-50">
+                        ${date.toString()}
+                    </span>
+                </p>
+            </div>
+        `;
+    }
+
+    msg.innerHTML = msgHtml;
+};
+
+const drawUsers = (ltUsers = []) => {
+    console.log('Llegada de los usuarios', ltUsers);
+    let liItems = '';
+
+    ltUsers.forEach(({image, uid, first_name, last_name}) => {
+        liItems += `
+            <li class='list-group-item profile'>
+                <!-- <div class='pr-image '>
+                    <img src='${!image ? '...' : image}' class='img-fluid' alt='' />
+                </div> -->
+                <div class='pr-data'>
+                    <span class="text-success text-uppercase">${first_name} ${last_name}</span>
+                    <span class="text-muted">${uid}</span>
+                </div>
+            </li>
+        `;
+    });
+
+    listUsers.innerHTML = liItems;
 };
 
 const init = async () => {
